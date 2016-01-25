5f9732137e57f2aa4b9df8ca43c54e8dce3c24c7
BenAychh <ben.tomas.hernandez@gmail.com>
2016-01-24 19:35:29 -0700

    changed to work with graphing.js

--- calculator.js
+++ calculator.js
@@ -1,12 +1,25 @@
-var math = require('./math.min.js');
-var Beautify = require('./beautifulDecimals');
-// var number = math.round(math.eval('cos(40 deg)'), 10);
-// console.log(number);
-// var beautified = new Beautify(number);
-// console.log(beautified.toString());
-
-function calculate(string) {
-  return math.eval(string);
+//var math = require('./math.min.js');
+//var Beautify = require('./beautifulDecimals');
+//var Graphing = require('./graphing.js')
+var canvas = document.getElementById('graphing');
+var grapher = new Graphing(canvas);
+function addLine() {
+  var equation = document.getElementById('equation').value;
+  var color = document.getElementById('color').value;
+  grapher.addLine(equation, color);
+  var deleteButtons = document.getElementById('lines');
+  var button = document.createElement('button');
+  button.innerHTML = 'delete: ' + equation;
+  button.style.backgroundColor = color;
+  button.data = equation;
+  button.onclick = removeLine;
+  deleteButtons.appendChild(button);
+}
+function removeLine() {
+  grapher.removeLine(this.data);
+  var deleteButtons = document.getElementById('lines');
+  deleteButtons.removeChild(this);
 }
 
-module.exports = calculate;
+
+//module.exports = calculate;
