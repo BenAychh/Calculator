3885b6de5017a3eb319a4e3cecf8bf4bd14f9be0
BenAychh <ben.tomas.hernandez@gmail.com>
2016-01-27 19:24:28 -0700

    Updated input to work with function notation

--- beautifyinput.js
+++ beautifyinput.js
@@ -1,56 +1,41 @@
-var Preview = {
+var UpdateInput = {
   delay: 150,        // delay after keystroke before updating
-
-  result: null,     // filled in by Init below
+  preview: null,     // filled in by Init below
   buffer: null,      // filled in by Init below
-
   timeout: null,     // store setTimout id
   mjRunning: false,  // true when MathJax is processing
   oldText: null,     // used to check if an update is needed
-
-  initialize: function() {
-    this.result = document.getElementById('result');
-    this.buffer = document.getElementById('buffer');
+  Start: function () {
+    this.preview = document.getElementById("result");
+    this.buffer = document.getElementById("buffer");
   },
-
-  swapBuffer: function() {
-    var buffer = this.preview;
-    var preview = this.buffer;
-    this.buffer = buffer;
-    this.preview = preview;
-    buffer.style.visibility = 'hidden';
-    buffer.style.position = 'absolute';
-    preview.style.visibility = 'visible';
-    preview.style.position = '';
-  },
-
-  update: function() {
-    if(this.timeout) {
+  Update: function () {
+    if (this.timeout) {
       clearTimeout(this.timeout);
     }
-    this.timeout = setTimeout(this.callback, this.delay);
+    this.timeout = setTimeout(this.callback,this.delay);
   },
-
-  createPreview: function() {
+  CreatePreview: function () {
     this.timeout = null;
     if (this.mjRunning) {
       return;
     }
-    var text = document.getElementById('expression');
-    if (text === this.oldText) {
+    var text = '`' + document.getElementById("expression").value + '`';
+    if (text === this.oldtext) {
       return;
     }
-    this.buffer.innerHTML = this.oldText = text;
+    this.buffer.innerHTML = this.oldtext = text;
     this.mjRunning = true;
-    MathJax.Hub.Queue(["Typeset", Mathjax.hub, this.buffer], ["PreviewDone", this]);
+    MathJax.Hub.Queue(
+      ["Typeset",MathJax.Hub,this.buffer],
+      ["DrawMath",this]
+    );
   },
-
-  previewDone: function() {
+  DrawMath: function () {
     this.mjRunning = false;
-    this.SwapBuffer();
+    this.preview.innerHTML = this.buffer.innerHTML;
   }
 
 };
-
-Preview.callback = MathJax.Callback(["CreatePreview",Preview]);
-Preview.callback.autoReset = true;  // make sure it can run more than once
+UpdateInput.callback = MathJax.Callback(["CreatePreview",UpdateInput]);
+UpdateInput.callback.autoReset = true;  // make sure it can run more than once
